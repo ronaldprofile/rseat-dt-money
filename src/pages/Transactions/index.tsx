dd708209bd7bf33e1a473bfccd2b21779e1c511f
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-08-09 09:11:31 -0300

    added table trasactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import * as T from "./styles";
+
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <T.TransactionContainer>
+        <T.TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento App Magalu</td>
+              <td>
+                <T.PriceHighlight variant="income">
+                  R$ 50.000,00
+                </T.PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>18/12/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Alomoço</td>
+              <td>
+                <T.PriceHighlight variant="outcome">
+                  - R$ 12,00
+                </T.PriceHighlight>
+              </td>
+              <td>alimentação</td>
+              <td>09/07/2022</td>
+            </tr>
+          </tbody>
+        </T.TransactionTable>
+      </T.TransactionContainer>
+    </div>
+  );
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const TransactionContainer = styled.main`
+  width: 100%;
+  padding: 0 1.5rem;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+`;
+
+export const TransactionTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${({ theme }) => theme["gray-700"]};
+    color: ${({ theme }) => theme["gray-300"]};
+    
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighlightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${({ variant, theme }) =>
+    variant === "income" ? theme["green-300"] : theme["red-300"]};
+`;
